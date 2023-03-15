b8a08d59d282383ab60594b03a15bcebbd83723b
SolomkaA <zolomung6@gmail.com>
2023-03-15 13:26:41 +0200

    Now OneSignal works with Android 13

--- app/src/main/java/com/example/ctrlabtest/MainApplication.kt
+++ app/src/main/java/com/example/ctrlabtest/MainApplication.kt
@@ -14,6 +14,7 @@ class MainApplication : Application() {
         // OneSignal Initialization
         OneSignal.initWithContext(this)
         OneSignal.setAppId(ONESIGNAL_APP_ID)
+        OneSignal.promptForPushNotifications()
     }
 
 }
